b5b58a9ccc5cac64fe748388debee33c71da22b2
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-06-24 22:03:38 -0700

    test

--- raffle_app/ios/Runner/AppDelegate.swift
+++ raffle_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,18 @@
 import Flutter
 import UIKit
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyBaLd0Yf0vC_Ys6ODhfSZKfElpAJ2iOFIA"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+@end
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
