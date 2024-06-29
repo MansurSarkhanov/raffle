1d7ec846731754881edd9c16d481ad830db8322a
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 <semralsaidiq067@gmail.com>
2024-06-29 15:04:53 +0400

    fix color

--- raffle_app/lib/features/campaing/data/model/campaing_model.dart
+++ raffle_app/lib/features/campaing/data/model/campaing_model.dart
@@ -2,13 +2,16 @@ class CampaingModel {
   String? image;
   String? price;
   String? date;
+  int? percent;
 
-  CampaingModel({this.image, this.price, this.date});
+  CampaingModel({this.image, this.price, this.date,this.percent});
 
   CampaingModel.fromJson(Map<String, dynamic> json) {
     image = json['image'];
     price = json['price'];
     date = json['date'];
+    percent = json['percent'];
+
   }
 
   Map<String, dynamic> toJson() {
@@ -16,6 +19,8 @@ class CampaingModel {
     data['image'] = image;
     data['price'] = price;
     data['date'] = date;
+    data['percent'] = percent;
+
     return data;
   }
 }
